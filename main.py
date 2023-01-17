7c35198886b6e0cde1ca625b6d22583292eef30a
unknown <bensandlin2@gmail.com>
2023-01-17 16:35:07 -0600

    Completed exercise 2.

--- main.py
+++ main.py
@@ -9,3 +9,8 @@
 # res = num1 * num2
 # print("The multiplication is", res)
 # Commented out exercise 1 for expediency
+
+#Exercise 2: Display three string “Name”, “Is”, “James” as “Name**Is**James”
+#Use the print() function to format the given words in the mentioned format. Display the ** separator between each string.
+#I honestly had to look at the solution for this. I'd never seen the sep function before. Neat new trick learned!
+print('My', 'Name', 'Is', 'James', sep='**')
