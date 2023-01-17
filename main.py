f44fe7246379e45d442d91fe94aa2200735f38af
unknown <bensandlin2@gmail.com>
2023-01-17 16:30:50 -0600

    Completed exercise 1.

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 #Continuing my practice with python
+#Exercise 1: Accept numbers from a user
+#Write a program to accept two numbers from the user and calculate multiplication
+#Basic stuff, input and what not.
+#Two numbers, so, two declarations
+num1 = int(input("Input the first number: "))
+num2 = int(input("Input the second number: "))
+#then multiply them together
+res = num1 * num2
+print("The multiplication is", res)
