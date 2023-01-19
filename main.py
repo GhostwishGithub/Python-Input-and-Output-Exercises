0ff48dcfa9f4c5dbe50972af953c91a4cd2cca9b
unknown <bensandlin2@gmail.com>
2023-01-19 16:37:49 -0600

    Commented out exercise 5.

--- main.py
+++ main.py
@@ -25,12 +25,13 @@ print('%o' % num)
 num = 458.541315
 print('%.2f' % num)
 
-#Exercise 5: Accept a list of 5 float numbers as an input from the user
-#Well that's back to basic simple. Loop, with float values and input. then stick em in a list and spit it back out
-results = []
-for x in range(1, 6):
-    print("Enter the number for space", x, ":")
-    userInput = float(input())
-    results.append(userInput)
+# #Exercise 5: Accept a list of 5 float numbers as an input from the user
+# #Well that's back to basic simple. Loop, with float values and input. then stick em in a list and spit it back out
+# results = []
+# for x in range(1, 6):
+#     print("Enter the number for space", x, ":")
+#     userInput = float(input())
+#     results.append(userInput)
 
-print("Here's the results:", results)
+# print("Here's the results:", results)
+#Commented out for expediency's sake
